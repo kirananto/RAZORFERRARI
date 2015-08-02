1e38e6580cf82c1b0a0a31788ae22f3378733fb8
kirananto <kirananto@gmail.com>
2015-08-02 22:11:42 +0530

    Upload to Dropbox

--- ferrari.sh
+++ ferrari.sh
@@ -67,6 +67,7 @@ echo -n "Enter The Zip file Name : "
 read zipfile
 echo $zipfile
 zip -r $zipfile tools META-INF -x *kernel/.gitignore*
+dropbox_uploader -p upload $MODULES_DIR/../FerrariOutput/$zipfile /
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
